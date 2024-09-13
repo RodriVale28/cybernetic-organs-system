3f201b492b2dde73957a9d4840354b15af356e85
RodriVale28 <rodrivalehernandez@gmail.com>
2024-09-12 20:48:05 -0500

    initial changes

--- src/main/java/com/cybernetic/Main.java
+++ src/main/java/com/cybernetic/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("this is assingment 1c");
+        System.out.println("this is still assignment 1c");
     }
 }
